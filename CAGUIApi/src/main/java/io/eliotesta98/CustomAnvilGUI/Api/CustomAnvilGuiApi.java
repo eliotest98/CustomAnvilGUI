8085c0b68e59f03fdffbcfc33e223ee02be38741
Elio Testa <eliotesta1998@libero.it>
2024-12-07 17:06:34 +0100

    Version 1.4
    Add compatibility with 1.21.4 minecraft version;
    Add setResult method in Api
    Bug Fix various;
    Code Optimizations;

--- CAGUIApi/src/main/java/io/eliotesta98/CustomAnvilGUI/Api/CustomAnvilGuiApi.java
+++ CAGUIApi/src/main/java/io/eliotesta98/CustomAnvilGUI/Api/CustomAnvilGuiApi.java
@@ -1,6 +1,11 @@
 package io.eliotesta98.CustomAnvilGUI.Api;
 
+import de.tr7zw.changeme.nbtapi.NBTItem;
 import io.eliotesta98.CustomAnvilGUI.Core.Main;
+import io.eliotesta98.CustomAnvilGUI.Interfaces.Interface;
+import io.eliotesta98.CustomAnvilGUI.Utils.DebugUtils;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 /**
  * CustomAnvilGui API.
@@ -13,14 +18,29 @@ public class CustomAnvilGuiApi {
     private static final boolean isDebugEnabled = Main.instance.getConfigGestion().getDebug().get("API");
 
     /**
-     * Return the sell price of a specific CubeGenerator with id and a multiplier.
+     * Set the result item in the interface.
+     * P. S. Without NbtApi, this method does not work!
      *
-     * @param generatorId generator id.
-     * @param multiplier  multiplier of price.
-     * @return the sell price.
+     * @param topInventory is the anvil inventory.
+     * @param levels       is the cost of enchanting.
+     * @param resultItem   is the ItemStack you want a set.
      */
-    public static double price(int generatorId, double multiplier) {
-        return 0.0;
+    public static void setResult(Inventory topInventory, int levels, ItemStack resultItem) {
+        DebugUtils debugUtils = new DebugUtils();
+        if (isDebugEnabled) {
+            debugUtils.addLine("setResult api method invocation");
+            debugUtils.addLine("Repair cost in levels: " + levels);
+            debugUtils.addLine("ItemStack to enchant:" + resultItem);
+        }
+        Interface interface_ = Main.instance.getConfigGestion().getInterfaces().get("Anvil");
+        interface_.setCostOfEnchant(topInventory, levels);
+
+        NBTItem nbtItem = new NBTItem(resultItem);
+        nbtItem.setInteger("ap.repairCost", levels);
+        topInventory.setItem(interface_.getImportantSlots().get("NoResult"), nbtItem.getItem());
+        if (isDebugEnabled) {
+            debugUtils.debug("setResult");
+        }
     }
 
 }
---
+++ CAGUIApi/src/main/resources/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"io.eliotesta98.CustomAnvilGUI.Api","l":"CustomAnvilGuiApi"}];updateSearchResults();
